dd14d0e755205bcdfe08576adf48eeb0838aed88
Adam Walter <adam.iverson.walter@gmail.com>
2017-08-24 15:08:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var name = "Joe"
 var height = "74"
 
 if (name === "Susan") {
-      console.log("The name is correct") }
-    else {
+      console.log("The name is correct")
+  }  else {
       console.console.log("Expect the name to be Susan");
-    }
+  }
 
 
 // Don't worry about this
